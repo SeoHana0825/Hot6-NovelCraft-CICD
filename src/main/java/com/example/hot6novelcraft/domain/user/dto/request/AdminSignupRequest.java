a53284f4deaaf1b9308a81ebe1681fd848d96acb
SeoHana0825 <onnuri1226@gmail.com>
2026-04-27 17:39:00 +0900

    refactor: 회원가입, SMS 임시토큰 탈취 방지 및 Redis 원자성 방지

--- src/main/java/com/example/hot6novelcraft/domain/user/dto/request/AdminSignupRequest.java
+++ src/main/java/com/example/hot6novelcraft/domain/user/dto/request/AdminSignupRequest.java
@@ -21,7 +21,10 @@ public record AdminSignupRequest(
         String phoneNo,
 
         @NotBlank(message = "휴대폰번호 인증이 필요합니다.")
-        String phoneToken
+        String phoneToken,
+
+        @NotBlank
+        String tempToken
 
 ) {
 
